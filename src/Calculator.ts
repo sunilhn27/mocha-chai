e2264ab63fc40fec10b329b42535b94008f70fe3
Sunil H N <sunilh.n27@gmail.com>
2023-11-30 00:32:57 +0530

    added calcualtor class

--- src/Calculator.ts
+++ src/Calculator.ts
@@ -6,4 +6,22 @@ class Calculator {
     }
 
 
+    sub(a: number, b: number): number {
+        return a - b;
+    }
+
+    mul(a: number, b: number): number {
+        return a * b;
+    }
+
+    div(a: number, b: number): number {
+        if (a / b == 0) {
+            throw new Error(" Zero cannot be divided")
+        }
+        return a + b;
+    }
+
+
 }
+
+export default Calculator;
